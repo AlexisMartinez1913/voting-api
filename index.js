214073034af406f1ceef60481ec5893f542171da
Alexis Martinez <jhonyalexis100@gmail.com>
2025-03-06 18:54:47 -0500

    Create enpoint - GET Voters by id

--- index.js
+++ index.js
@@ -43,3 +43,15 @@ app.get('/voters', async (req, res) => {
         res.status(500).json({error: err.message})
     }
 })
+
+//GET /voters/:id Detalles votantes
+app.get('/voters/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const result = await pool.query('SELECT * FROM voters WHERE id = $1', [id])
+        if (result.rows.length === 0) return res.status(404).json({ error: 'Votante NO ENCONTRADO'})
+        res.json(result.rows[0])
+    } catch (err) {
+        res.status(500).json({ error: err.message})
+    }
+})
