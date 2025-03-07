b38afc39a088372da6994163e26d868e6a78861a
Alexis Martinez <jhonyalexis100@gmail.com>
2025-03-06 21:18:18 -0500

    Create enpoint - GET votes/statistics

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.get('/candidates', async (req, res) => {
 app.get('/candidates/:id', async (req, res) => {
     const { id } = req.params
     try {
-        const result = await pool.query('SELECT * FROM WHERE id = $1', [id])
+        const result = await pool.query('SELECT * FROM candidates WHERE id = $1', [id])
         if (result.rows.length === 0) {
             return res.status(404).json({ error: 'Candidato NO ENCONTRADO'})
         }
