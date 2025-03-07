1e7826e7f16bb2b09fddcc0b8f3e46a355e4341a
Alexis Martinez <jhonyalexis100@gmail.com>
2025-03-06 20:34:30 -0500

    Create enpoint - DELETE candidate

--- index.js
+++ index.js
@@ -118,6 +118,21 @@ app.get('/candidates/:id', async (req, res) => {
     }
 })
 
+//DELETE /candidates/:id
+app.delete('/candidates/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const result = await pool.query('DELETE FROM candidates WHERE id = $1 RETURNING *', [id])
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'Candidato NO ENCONTRADO'})
+        }
+        res.json({ message: 'Candidato ELIMINADO', candidate: result.rows[0]})
+    } catch (err) {
+        res.status(500).json({ error: err.message})
+    }
+})
+
+
 
 //iniciar el servidor
 app.listen(port, () => {
